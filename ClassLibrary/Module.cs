2d20be5e78e7c525acd73774637f170801ba23c4
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2019-04-18 17:37:20 +0300

    nullable reference types demo

---
+++ ClassLibrary/Module.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+namespace ClassLibrary
+{
+    public static class Module
+    {
+        public static int Length (string s) => s.Length;
+        public static void LogLength (string value) => WriteLine (Length (value));
+
+        public static int? LengthUnsafe (string? s) => s?.Length;
+        public static void LogLengthUnsafe (string? value) => WriteLine (LengthUnsafe (value));
+    }
+}
--- ConsoleApp/NullableReferenceTypes.cs
+++ ConsoleApp/NullableReferenceTypes.cs
@@ -1,10 +1,57 @@
-﻿namespace ConsoleApp
+﻿using System;
+using ClassLibrary;
+using static System.Console;
+
+namespace ConsoleApp
 {
     internal static class NullableReferenceTypes
     {
-        public static void Demo ()
+        public static void Demo () => Demo1 (); // of 5
+
+        static void Demo1 ()
         {
-            // Improvise :)
+            LogDoubleLength ("12345");
+            LogDoubleLength ("1234567890");
+        }
+
+        static void LogDoubleLength (string value)
+        {
+            int length = Length (value);
+            int doubleLength = 2 * length;
+            WriteLine (doubleLength);
+        }
+
+        static int Length (string s) => s.Length;
+
+        static void Demo2 ()
+        {
+            LogLength ("12345");
+            // TODO: LogLength (null);
+        }
+
+        static void LogLength (string value) => WriteLine (Length (value));
+
+        static void Demo3 ()
+        {
+            string? format = null;
+            var message = string.Format (format, 1);
+
+            WriteLine (message);
+        }
+
+        static void Demo4 ()
+        {
+            Module.LogLength ("12345");
+            // TODO: Module.LogLength (null);
+        }
+
+        static void Demo5 ()
+        {
+            #nullable disable
+
+            Module.LogLength (null);
+
+            #nullable restore
         }
     }
 }
